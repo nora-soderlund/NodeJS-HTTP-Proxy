65b105ea7fe81293c87dc2ac53be6d30166ba202
Nora Salehson <wn00202145@wgs.wuerth.com>
2022-11-08 17:21:42 +0100

    Added error handling

--- index.js
+++ index.js
@@ -1,49 +1,97 @@
 import http from "http";
 import httpProxy from "http-proxy";
 
+import { URL } from "url";
+
+import fs, { write } from "fs";
+
+const config = JSON.parse(fs.readFileSync("./config.json"));
+
 const proxy = httpProxy.createProxyServer({});
+
+function writeError(request, name, error) {
+    try {
+        console.error(`ERROR: ${error}`);
+    
+        fs.writeFileSync(`${config.logs}/${name}_${Date.now()}.json`, JSON.stringify({
+            request: {
+                headers: request.headers,
+                url: request.url
+            },
+            error
+        }));
+    }
+    catch(error) {
+        console.error(`FAILED TO WRITE ERROR: ${error}`);
+    }
+};
+
+proxy.on("error", (error, request, response) => {
+    writeError(request, "proxy_error", error);
+
+    try {
+        response.writeHead(500);
+    
+        response.end();
+    }
+    catch(error) {
+        writeError(request, "proxy_error", "Failed to end response!");
+    }
+});
  
 const server = http.createServer((request, response) => {
-    const method = request.method;
-    const remoteAddress = request.socket.remoteAddress;
+    try {
+        const method = request.method;
+        const remoteAddress = request.socket.remoteAddress;
 
-    const host = request.headers.host;
-    const url = request.url.toLowerCase();
+        const url = new URL(request.url, `http://${request.headers.host}`);
 
-    console.log(`${remoteAddress} ${method} ${url} for ${host} ${url}`);
+        if(!url.hostname || !url.hostname.length) {
+            console.warn(`${remoteAddress} ${method} ${url.href}: missing HOST header`);
 
-    if(!host || !host.length) {
-        console.warn(`${remoteAddress} ${method} ${url}: missing HOST header`);
+            writeError(request, "server_error", "Missing HOST header!");
+
+            return response.end();
+        }
+
+        const subhost = url.hostname.substring(0, url.hostname.indexOf('.'));
+
+        // serve public index
+        if(config.allowedHosts.includes(url.hostname) || ([ "www" ]).includes(subhost)) {
+            console.log(`${remoteAddress} ${method} ${url.href}: serving public index`);
+
+            return proxy.web(request, response, { target: "http://localhost:81" });
+        }
+        
+        // serve public api
+        if(([ "api" ]).includes(subhost)) {
+            console.log(`${remoteAddress} ${method} ${url.href}: serving public api`);
+
+            return proxy.web(request, response, { target: "http://localhost:81" });
+        }
+        
+        // serve demo api
+        if(([ "demo" ]).includes(subhost)) {
+            console.log(`${remoteAddress} ${method} ${url.href}: serving demo api`);
+
+            return proxy.web(request, response, { target: "http://localhost:82" });
+        }
+
+        if(config.allowUnknownHost) {
+            console.log(`${remoteAddress} ${method} ${url.href}: serving public index with allowUnknownHosts rule`);
+
+            return proxy.web(request, response, { target: "http://localhost:81" });
+        }
+
+        console.log(`${remoteAddress} ${method} ${url.href}: missing handler!`);
+
+        writeError(request, "server_error", "Missing handler!");
 
         return response.end();
     }
-
-    const subhost = host.substring(0, host.indexOf('.'));
-
-    // serve public index
-    if(([ "ridetracker.app", "localhost" ]).includes(host) || ([ "www" ]).includes(subhost)) {
-        console.log(`${remoteAddress} ${method} ${url}: serving public index`);
-
-        return proxy.web(request, response, { target: "http://localhost:81" });
+    catch(error) {
+        writeError(request, "server_error", error);
     }
-    
-    // serve public api
-    if(([ "api" ]).includes(subhost)) {
-        console.log(`${remoteAddress} ${method} ${url}: serving public api`);
-
-        return proxy.web(request, response, { target: "http://localhost:81" });
-    }
-    
-    // serve demo api
-    if(([ "demo" ]).includes(subhost)) {
-        console.log(`${remoteAddress} ${method} ${url}: serving demo api`);
-
-        return proxy.web(request, response, { target: "http://localhost:82" });
-    }
-
-    console.log(`${remoteAddress} ${method} ${url}: missing handler!`);
-
-    response.end();
 });
  
 server.listen(80);
